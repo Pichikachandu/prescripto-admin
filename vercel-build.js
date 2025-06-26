c1c9d862cbf63cef2c97552a54ccbbac427c2468
Pichikachandu <pichikachandu@gmail.com>
2025-06-27 01:58:32 +0530

    Clean up Vercel configuration and remove unused build script

--- vercel-build.js
+++
@@ -1,8 +0,0 @@
-// This script helps Vercel build the project correctly
-const { execSync } = require('child_process');
-
-console.log('Installing dependencies...');
-execSync('npm install --legacy-peer-deps', { stdio: 'inherit' });
-
-console.log('Running build...');
-execSync('npm run build', { stdio: 'inherit' });
